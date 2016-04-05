dd4df02166c6b43f551d1def5db3fd899b8898c3
Mihhail Sidorin <msidorin@gmail.com>
2016-04-05 05:34:25 +0300

    Allow FactoryGirl syntax in specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,8 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rspec'
 require 'capybara/rails'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
